14209e2e33e6eba4e66e79bab490f85eac5b9903
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2024-03-21 13:04:15 -0500

    Set up the onClick method in the ArticleAdapter

--- app/src/main/java/com/codepath/articlesearch/ArticleAdapter.kt
+++ app/src/main/java/com/codepath/articlesearch/ArticleAdapter.kt
@@ -50,9 +50,18 @@ class ArticleAdapter(private val context: Context, private val articles: List<Ar
         }
 
         override fun onClick(v: View?) {
-            // TODO: Get selected article
+            // The first thing we will want to do in passing along the article information is to get the selected article from the array
+            val article = articles[absoluteAdapterPosition]
 
-            // TODO: Navigate to Details screen and pass selected article
+            // Navigate to Details screen and pass selected article
+            // Next, we want to send the app to the details screen, along with information about the article that was clicked on. We can do this in 3 steps:
+            //  1. We can create an Intent, using the current context, and the Class that we are navigating to.
+            val intent = Intent(context, DetailActivity::class.java)
+            //  2. We can use .putExtra to pass along the article, with the first argument being the key for the data, and the next being article itself.
+            intent.putExtra(ARTICLE_EXTRA, article)
+            //  3. We can then call startActivity with the current context, using the intent as its param
+            // Implicit Intents are requests to perform an action based on a desired action and target data
+            context.startActivity(intent)
         }
     }
 }
